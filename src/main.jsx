831aa9902eecf992a0d8ce74baa6cc86b78aa2c9
Sachin Kumar <ks900137@gmail.com>
2026-09-14 19:52:50 +0530

    Add main component to render SalesDashboard

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SalesDashboard from "../sales-dashboard(3).jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SalesDashboard />
+  </React.StrictMode>
+);
